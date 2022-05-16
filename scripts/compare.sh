ffc7fb0f90c5f4c7acf37ef598116ca4ab136824
Abdulmohaimen <46815218+Abdulmohaimen-devops@users.noreply.github.com>
2022-05-16 21:15:29 +0200

    v

---
+++ scripts/compare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+sha1sum   /opt/SampleMavenTomcatApp.war |  awk '{ print $1 }' > /opt/shaDeploy
+sha1sum   /opt/SampleMavenTomcatApp.war |  awk '{ print $1 }'
+
+build_sha=$(cat /opt/shaBuild)
+deploy_sha=$(cat /opt/shaDeploy)
+
+if [ $build_sha = $deploy_sha ]
+then
+    echo Deployment successed, Go for testing
+else
+    echo Deployment failed, trigger Pipeline again
+    aws codepipeline start-pipeline-execution --name sha
+fi
