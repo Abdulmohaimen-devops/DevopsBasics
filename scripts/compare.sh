3653bea4043c88d9ab0e0ea0eb0ca5a311f03237
Abdulmohaimen <46815218+Abdulmohaimen-devops@users.noreply.github.com>
2022-05-16 22:08:52 +0200

    v

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp target/SampleMavenTomcatApp.war /opt/
-cp target/shaBuild /opt/
+cp target/SampleMavenTomcatApp.war /opt/SampleMavenTomcatApp.war
+cp target/shaBuild /opt/shaBuild
 
 sha1sum   /opt/SampleMavenTomcatApp.war |  awk '{ print $1 }' > /opt/shaDeploy
 sha1sum   /opt/SampleMavenTomcatApp.war |  awk '{ print $1 }'
