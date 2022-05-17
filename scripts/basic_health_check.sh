88a7b4558ad2296f78604efff46399a9800d8d5e
Abdulmohaimen <46815218+Abdulmohaimen-devops@users.noreply.github.com>
2022-05-17 05:27:27 +0200

    g

--- scripts/basic_health_check.sh
+++ scripts/basic_health_check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf /opt/SampleMavenTomcatApp.war
+
 for i in `seq 1 10`;
 do
   HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:8080`
@@ -13,4 +15,3 @@ done
 echo "Server did not come up after expected time. Failing."
 exit 1
 
-rm /opt/SampleMavenTomcatApp.war
